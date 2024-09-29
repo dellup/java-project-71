c5bd802f95bde351cce72b737ff6e8e3e5024133
Danil Zenin <zenindanil2000@gmail.com>
2024-09-29 12:12:29 +0300

    add picocli default param

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
-
-class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Runnable{
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
         System.out.println("Hello, World!");
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+
     }
 }
