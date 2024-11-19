32356daa8a71972405d8796a07f84b188499f5de
Danil Zenin <zenindanil2000@gmail.com>
2024-11-19 10:49:02 +0300

    fix pypeline bug

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,9 @@
 package hexlet.code;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.TreeMap;
 
 import static hexlet.code.App.getFormat;
@@ -11,21 +14,18 @@ import static hexlet.code.cnst.Type.YAML;
 
 public class Differ {
     public static String generate(String filepathFirst, String filepathSecond, String format) throws Exception {
-        TreeMap<String, Object> mapFirst;
-        TreeMap<String, Object> mapSecond;
-        if (filepathFirst.endsWith("json")) {
-            mapFirst = new TreeMap<>(getData(filepathFirst, JSON));
-        } else {
-            mapFirst = new TreeMap<>(getData(filepathFirst, YAML));
-        }
-        if (filepathSecond.endsWith("json")) {
-            mapSecond = new TreeMap<>(getData(filepathSecond, JSON));
-        } else {
-            mapSecond = new TreeMap<>(getData(filepathSecond, YAML));
-        }
-        String gen = Formatter.format(mapFirst, mapSecond, getFormat());
+        String contentFirst = isPath(filepathFirst) ? Files.readString(Path.of(filepathFirst)) : filepathFirst;
+        String contentSecond = isPath(filepathSecond) ? Files.readString(Path.of(filepathSecond)) : filepathSecond;
+        Map<String, Object> mapFirst = getData(contentFirst, filepathFirst.endsWith("json") ? JSON : YAML);
+        Map<String, Object> mapSecond = getData(contentSecond, filepathSecond.endsWith("json") ? JSON : YAML);
+        TreeMap<String, Object> treeFirst = new TreeMap<>(mapFirst);
+        TreeMap<String, Object> treeSecond = new TreeMap<>(mapSecond);
+        String gen = Formatter.format(treeFirst, treeSecond, format);
         System.out.println(gen);
         return gen;
+    }
+    private static boolean isPath(String input) {
+        return Files.exists(Path.of(input));
     }
     public static String generate(String filepathFirst, String filepathSecond) throws Exception {
         return generate(filepathFirst, filepathSecond, getFormat());
