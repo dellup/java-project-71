90abc61f60061c5a059b26f4cc7ab029b4f39bfb
Danil Zenin <zenindanil2000@gmail.com>
2024-11-26 01:02:23 +0300

    fix pypeline

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,25 +1,19 @@
 package hexlet.code.formatters;
 
-import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.IOException;
+
 import java.util.TreeMap;
 
 public class Json extends Format {
-    public String generate(TreeMap<String, Object> map1, TreeMap<String, Object> map2) throws IOException {
+    @Override
+    public String generate(TreeMap<String, Object> map1, TreeMap<String, Object> map2) throws JsonProcessingException {
         super.init(map1, map2);
+        TreeMap<String, Object> result = new TreeMap<>();
+        result.put("After", plus);
+        result.put("Before", minus);
+        result.put("No changes", noDiff);
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
-        str.append("Before: ").append("\n");
-        str.append(minus).append("\n");
-        objectMapper.writeValue(new File("src/test/resources/fixtures/minus.json"), minus);
-        str.append("After: ").append("\n");
-        str.append(plus).append("\n");
-        objectMapper.writeValue(new File("src/test/resources/fixtures/plus.json"), plus);
-        str.append("No changes: ").append("\n");
-        str.append(noDiff);
-        objectMapper.writeValue(new File("src/test/resources/fixtures/noDiff.json"), noDiff);
-        return str.toString();
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
     }
 }
--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -99,12 +99,25 @@ public class AppTest {
         String strYaml2 = readFile(new File(yaml2));
         TreeMap<String, Object> map1 = new TreeMap<>(getData(strYaml1, "yaml"));
         TreeMap<String, Object> map2 = new TreeMap<>(getData(strYaml2, "yaml"));
-        String expected = "Before: \n"
-                + "{follow=false, proxy=123.234.53.22, timeout=50}\n"
-                + "After: \n"
-                + "{obj1={nestedKey=value, isNested=true}, timeout=20, verbose=true}\n"
-                + "No changes: \n"
-                + "{host=hexlet.io}";
+        String expected = "{\n"
+                          + "  \"After\" : {\n"
+                          + "    \"obj1\" : {\n"
+                          + "      \"nestedKey\" : \"value\",\n"
+                          + "      \"isNested\" : true\n"
+                          + "    },\n"
+                          + "    \"timeout\" : 20,\n"
+                          + "    \"verbose\" : true\n"
+                          + "  },\n"
+                          + "  \"Before\" : {\n"
+                          + "    \"follow\" : false,\n"
+                          + "    \"proxy\" : \"123.234.53.22\",\n"
+                          + "    \"timeout\" : 50\n"
+                          + "  },\n"
+                          + "  \"No changes\" : {\n"
+                          + "    \"host\" : \"hexlet.io\"\n"
+                          + "  }\n"
+                          + "}";
+
         String actual = Formatter.format(map1, map2, JSON);
         assertEquals(expected, actual);
     }
